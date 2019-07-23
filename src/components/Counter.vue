fec825884a926e4a1870eab246ee2d8d6e231041
Ethan <ethan.huang@oocl.com>
2019-07-23 17:50:36 +0800

    Refactor: delete the extra code

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,7 @@
 <template>
     <div id="Counter">
         <table>
-            <!-- <ul>{{counter}}</ul> -->
-            <ul>{{this.$store.state.allCounter}}</ul>
-            <ul>{{this.$store.state.allCounter[this.index]}}</ul>
             <ul>{{this.$store.state.allCounter[this.index].counter}}</ul>
-            <ul>index:{{index}}</ul>
             <ul>
                 <button @click="addCounter">点我+</button>
                 <button @click="reduceCounter">点我-</button>
@@ -26,14 +22,10 @@
         },
         methods:{
             addCounter:function () {
-                console.log('Counter index:',this.index);
-                //this.counter ++;
                 this.$store.commit("addCounter",this.index)
                 this.$store.commit("countSum")
-                console.log("111111111111111")
             },
             reduceCounter:function () {
-                //this.counter --;
                 this.$store.commit("reduceCounter",this.index)
                 this.$store.commit("countSum")
             }
